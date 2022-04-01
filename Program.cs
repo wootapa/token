69d3a1ab77401af826f0eca123e866b155a12b5c
Andreas Petersson <andreas.peterson@consid.se>
2022-04-01 20:19:09 +0200

    Print payload. Add progress.

--- Program.cs
+++ Program.cs
@@ -4,21 +4,25 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
+using System.Threading.Tasks;
 
 try
 {
+    // Arguments
     var url = args.FirstOrDefault();
     var clientId = args.Skip(1)?.FirstOrDefault();
     var secret = args.Skip(2)?.FirstOrDefault();
+    bool printPayload = args.Skip(3)?.FirstOrDefault()?.Equals("--decode") ?? false;
 
     if (url is null ||
         clientId is null ||
         secret is null)
     {
-        Console.WriteLine("Usage: token.exe <token_url> <client_id> <secret>");
+        Console.WriteLine("Usage: token.exe <token_url> <client_id> <secret> --decode (optional)");
         Environment.Exit(1);
     }
 
+    // Setup request
     var client = new HttpClient();
     var tokenRequest = new HttpRequestMessage(HttpMethod.Post, url)
     {
@@ -29,12 +33,56 @@ try
     };
     tokenRequest.Headers.Add("Authorization", "Basic " + Convert.ToBase64String(Encoding.Default.GetBytes(clientId + ":" + secret)));
 
-    using var responseMessage = client.Send(tokenRequest);
-    var response = responseMessage.Content.ReadAsStream();
-    var accessToken = JsonSerializer.Deserialize<OAuthResponse>(response).access_token;
+    // Create tasks - request and progress-spinner
+    var task1 = client.SendAsync(tokenRequest);
+    var task2 = Task.Run(async () =>
+    {
+        Console.CursorVisible = false;
+        int i = 0;
 
+        while (!task1.IsCompleted)
+        {
+            switch (i++ % 4)
+            {
+                case 0: Console.Write("\r|"); break;
+                case 1: Console.Write("\r/"); break;
+                case 2: Console.Write("\r-"); break;
+                case 3: Console.Write("\r\\"); break;
+            }
+            await Task.Delay(100);
+        }
+        Console.Write("\r"); 
+        Console.CursorVisible = true;
+    });
+
+    // Wait...
+    await Task.WhenAll(new List<Task> { task1, task2 });
+
+    // Print token
+    using var response = task1.Result.Content.ReadAsStream();
+    var accessToken = JsonSerializer.Deserialize<OAuthResponse>(response).access_token;
     Console.ForegroundColor = ConsoleColor.Blue;
     Console.WriteLine(accessToken);
+
+    // Print payload?
+    if (printPayload)
+    {
+        var payload = accessToken.Split('.')[1].Replace('_', '/').Replace('-', '+');
+        switch (payload.Length % 4)
+        {
+            case 2: payload += "=="; break;
+            case 3: payload += "="; break;
+        }
+        var payloadDecoded = Encoding.Default.GetString(Convert.FromBase64String(payload));
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        var jsonElement = JsonSerializer.Deserialize<JsonElement>(payloadDecoded);
+        var payloadPretty = JsonSerializer.Serialize(jsonElement, options);
+
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(payloadPretty);
+    }
+
     Environment.Exit(0);
 }
 catch (Exception ex)
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# token - prints accesstoken
+# token - prints accesstoken (OAuth Client Credentials Flow)
 
 ```Shell
-docker run --rm wootapa/token "<token_url>" "<client_id>" "<secret>"
+docker run --rm wootapa/token "<token_url>" "<client_id>" "<secret>" --decode
 ```
+
+`--decode` is optional and prints the decoded payload
